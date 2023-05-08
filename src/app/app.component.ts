bc5f3ae1051a80407bc832054b91c002f3b7f6c8
benjamin <benja@gmail.com>
2023-05-08 17:10:05 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Item} from './item';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app';
+
+filter: 'all' | 'active' | 'done' = 'all';
+
+allItems =[
+  {description: 'eat', done: true},
+  {description: 'sleep', done: false},
+  {description: 'play', done: false},
+  {description: 'laugh', done: false},
+];
+get items() {
+  if (this.filter==='all'){
+    return this.allItems;
+  }
+  return this.allItems.filter((item)=>this.filter==='done' ? item.done : !item.done);
+}
+
+addItem(description: string) {
+  this.allItems.unshift({
+    description,
+    done:false
+  });
+}
+remove(item:Item) {
+  this.allItems.splice(this.allItems.indexOf(item), 1);
+}
 }
